0c9ddbb4272ccae66b8bd86c70ad3ec453133122
ShiftainAhmad <AHMADSIFTAIN89@GMAIL.COM>
2025-07-11 10:56:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App()
     <div className="m-4">
       <h1 className="">Hello Next.js</h1>
       <p>Count: {count}</p>
-      <button onClick={handleClick} className="bg-black border-black text-white px-2 py-2 rounded-full mt-2 hover:bg-gray-500">Incremen</button>
+      <button onClick={handleClick} className="bg-black border-black text-white px-2 py-2 rounded-full mt-2 hover:bg-gray-500">Increment</button>
     </div>
   )
 }
